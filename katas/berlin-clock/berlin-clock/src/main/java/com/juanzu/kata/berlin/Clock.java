2c827a43e3da80dd1334472bee120f53a8985f69
juazugas <juazuri@gmail.com>
2017-04-25 10:19:15 +0200

    Second and Third top rows are red and mark the hour.

--- katas/berlin-clock/berlin-clock/src/main/java/com/juanzu/kata/berlin/Clock.java
+++ katas/berlin-clock/berlin-clock/src/main/java/com/juanzu/kata/berlin/Clock.java
@@ -9,6 +9,12 @@ import java.util.List;
  */
 public class Clock {
 
+	private static final int THIRD_ROW_RANGE = 5;
+	private static final int HOUR_FIVE = 5;
+	private static final int HOUR_TEN = 10;
+	private static final int HOUR_FIFTEEN = 15;
+	private static final int HOUR_TWENTY = 20;
+
 	private int second;
 	private int minute;
 	private int hour;
@@ -24,11 +30,45 @@ public class Clock {
 	}
 
 	public List<Led> getSecondRow() {
+		List<Led> hourRow = initHourRow();
+		if (hour>=HOUR_FIVE) {
+			setLamp(hourRow, 0, Led.RED);
+		} 
+		if (hour>=HOUR_TEN) {
+			setLamp(hourRow, 1, Led.RED);
+		}
+		if (hour>=HOUR_FIFTEEN) {
+			setLamp(hourRow, 2, Led.RED);
+		}
+		if (hour>=HOUR_TWENTY) {
+			setLamp(hourRow, 3, Led.RED);
+		}
+		return hourRow;
+	}
+
+	public List<Led> getThirdRow() {
+		List<Led> hourRow = initHourRow();
+		if (hour%THIRD_ROW_RANGE>=1) {
+			setLamp(hourRow, 0, Led.RED);
+		}
+		if (hour%THIRD_ROW_RANGE>=2) {
+			setLamp(hourRow, 1, Led.RED);
+		}
+		if (hour%THIRD_ROW_RANGE>=3) {
+			setLamp(hourRow, 2, Led.RED);
+		}
+		if (hour%THIRD_ROW_RANGE>=4) {
+			setLamp(hourRow, 3, Led.RED);
+		}
+		return hourRow;
+	}
+	
+	private List<Led> initHourRow() {
 		return Arrays.asList(Led.OFF, Led.OFF, Led.OFF, Led.OFF);
 	}
 	
-	public List<Led> getThirdRow() {
-		return Arrays.asList(Led.OFF, Led.OFF, Led.OFF, Led.OFF);
+	private void setLamp(List<Led> row, int index, Led color) {
+		row.set(index, color);
 	}
 	
 }
--- katas/berlin-clock/berlin-clock/src/test/java/com/juanzu/kata/berlin/ClockTest.java
+++ katas/berlin-clock/berlin-clock/src/test/java/com/juanzu/kata/berlin/ClockTest.java
@@ -75,12 +75,124 @@ public class ClockTest {
 			assertThat(row).size().isEqualTo(4);
 	}
 	
-	@Test 
-	public void should_second_row_represent_lamp_every_five_hours () {
-		List<Led> rowFirstFive = null;
+	@Test
+	public void should_2nd_row_first_lamp_turn_on_after_5_hours () {
 		_given: _when: _then: {
-			
+			for (int i = 0; i < 24; i++) {
+				Clock dayClock = new Clock(0, 0, i);
+				List<Led> secondRow = dayClock.getSecondRow();
+				if(i<5){
+					assertThat(secondRow.get(0)).isEqualTo(Led.OFF);
+				} else {
+					assertThat(secondRow.get(0)).isEqualTo(Led.RED);
+				}
+			}
 		}
+	}
+	
+	@Test 
+	public void should_2nd_row_2nd_lamp_turn_on_after_10_hours () {
+		_given: _when: _then: {
+			for (int i = 0; i < 24; i++) {
+				Clock dayClock = new Clock(0, 0, i);
+				List<Led> secondRow = dayClock.getSecondRow();
+				if(i<10){
+					assertThat(secondRow.get(1)).isEqualTo(Led.OFF);
+				} else {
+					assertThat(secondRow.get(1)).isEqualTo(Led.RED);
+				}
+			}
+		}	
+	}
+
+	@Test 
+	public void should_2nd_row_3rd_lamp_turn_on_after_15_hours () {
+		_given: _when: _then: {
+			for (int i = 0; i < 24; i++) {
+				Clock dayClock = new Clock(0, 0, i);
+				List<Led> secondRow = dayClock.getSecondRow();
+				if(i<15){
+					assertThat(secondRow.get(2)).isEqualTo(Led.OFF);
+				} else {
+					assertThat(secondRow.get(2)).isEqualTo(Led.RED);
+				}
+			}
+		}	
+	}
+
+	@Test 
+	public void should_2nd_row_4th_lamp_turn_on_after_20_hours () {
+		_given: _when: _then: {
+			for (int i = 0; i < 24; i++) {
+				Clock dayClock = new Clock(0, 0, i);
+				List<Led> secondRow = dayClock.getSecondRow();
+				if(i<20){
+					assertThat(secondRow.get(3)).isEqualTo(Led.OFF);
+				} else {
+					assertThat(secondRow.get(3)).isEqualTo(Led.RED);
+				}
+			}
+		}	
+	}
+	
+	@Test
+	public void should_3rd_row_1st_lamp_turn_on_after_1_hour () {
+		_given: _when: _then: {
+			for (int i = 0; i < 24; i++) {
+				Clock dayClock = new Clock(0, 0, i);
+				List<Led> thirdRow = dayClock.getThirdRow();
+				if(i%5==0){
+					assertThat(thirdRow.get(0)).isEqualTo(Led.OFF);
+				} else {
+					assertThat(thirdRow.get(0)).isEqualTo(Led.RED);
+				}
+			}
+		}	
+	}	
+
+	@Test
+	public void should_3rd_row_2nd_lamp_turn_on_after_2_hour () {
+		_given: _when: _then: {
+			for (int i = 0; i < 24; i++) {
+				Clock dayClock = new Clock(0, 0, i);
+				List<Led> thirdRow = dayClock.getThirdRow();
+				if(i%5<2){
+					assertThat(thirdRow.get(1)).isEqualTo(Led.OFF);
+				} else {
+					assertThat(thirdRow.get(1)).isEqualTo(Led.RED);
+				}
+			}
+		}	
+	}
+
+	@Test
+	public void should_3rd_row_3rd_lamp_turn_on_after_3_hour () {
+		_given: _when: _then: {
+			for (int i = 0; i < 24; i++) {
+				Clock dayClock = new Clock(0, 0, i);
+				List<Led> thirdRow = dayClock.getThirdRow();
+				if(i%5<3){
+					assertThat(thirdRow.get(2)).isEqualTo(Led.OFF);
+				} else {
+					assertThat(thirdRow.get(2)).isEqualTo(Led.RED);
+				}
+			}
+		}	
+	}
+
+	@Test
+	public void should_3rd_row_4th_lamp_turn_on_after_4_hour () {
+		_given: _when: _then: {
+			for (int i = 0; i < 24; i++) {
+				Clock dayClock = new Clock(0, 0, i);
+				List<Led> thirdRow = dayClock.getThirdRow();
+				if(i%5<4){
+					assertThat(thirdRow.get(3)).isEqualTo(Led.OFF);
+				} else {
+					assertThat(thirdRow.get(3)).isEqualTo(Led.RED);
+				}
+			}
+		}	
 	}
 
 }
